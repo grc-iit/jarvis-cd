cc60c8cc8a5729e8f0f666e2a0fda8077a5d2dc8
lukemartinlogan <lukemartinlogan@gmail.com>
2022-05-20 13:13:26 -0500

    YCSB GetRuntime

--- jarvis_cd/workloads/ycsb.py
+++ jarvis_cd/workloads/ycsb.py
@@ -10,7 +10,7 @@ class YCSB(ExecNode):
         super().__init__(name, cmd, print_output, collect_output, affinity, sleep_period_ms, max_retries)
 
     def GetRuntime(self):
-        for host, line in self.output:
+        for host, line in self.output.items():
             grp = re.match("\[OVERALL\], RunTime\(ms\), ([0-9]+)", line)
             if grp:
                 return float(grp.group(1))
