0b61e998456aa364889b7e090f2b7826649f77be
lukemartinlogan <lukemartinlogan@gmail.com>
2022-07-06 15:02:40 -0500

    Add name field

--- jarvis_cd/spack/link_package.py
+++ jarvis_cd/spack/link_package.py
@@ -2,11 +2,11 @@
 from jarvis_cd.comm.ssh_node import SSHNode
 
 class LinkSpackage(SSHNode):
-    def __init__(self, hosts, spack_query_dict, link_path,
+    def __init__(self, name, hosts, spack_query_dict, link_path,
                  username=None, pkey=None, password=None, port=22,
                  sudo=False, print_output=True, collect_output=True, do_ssh=True):
         self.spack_query_dict = spack_query_dict
         package_name = self.spack_query_dict['package_name']
         spack_query = package_name
         cmds = f"ln -s `spack find  --format \"{{prefix}}\" {spack_query}` {link_path}"
-        super().__init__(hosts, cmds, username, pkey, password, port, sudo, print_output, collect_output, do_ssh)
+        super().__init__(name, hosts, cmds, username, pkey, password, port, sudo, print_output, collect_output, do_ssh)
