5591bb986f02aadea64bc20c58287d0292f630be
lukemartinlogan <lukemartinlogan@gmail.com>
2022-07-08 20:17:17 -0500

    Allow ssh to specific host jarvis-bootstrap

--- jarvis_cd/bootstrap/ssh.py
+++ jarvis_cd/bootstrap/ssh.py
@@ -2,9 +2,10 @@ from jarvis_cd.basic.exec_node import ExecNode
 from jarvis_cd.bootstrap.ssh_args import SSHArgs
 
 class JarvisSSH(SSHArgs):
-    def __init__(self, conf):
+    def __init__(self, conf, host_id=0):
         self.conf = conf
+        self.host_id = host_id
         self.ParseSSHArgs()
 
     def Run(self):
-        ExecNode('Do SSH', f'ssh -p {self.port} {self.username}@{self.hosts[0]}', collect_output=False).Run()
+        ExecNode('Do SSH', f'ssh -p {self.port} {self.username}@{self.hosts[self.host_id]}', collect_output=False).Run()
