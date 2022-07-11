e5f3d74d87f9da009590678d28b08ad0f1aa3b4d
lukemartinlogan <lukemartinlogan@gmail.com>
2022-07-10 20:21:29 -0500

    Spack remove query

--- jarvis_cd/bootstrap/spack_setup.py
+++ jarvis_cd/bootstrap/spack_setup.py
@@ -8,14 +8,14 @@ class SpackSetup(Package):
     def _LocalInstall(self):
         spack_root = self.config['spack']['path']
         GitNode(**self.config['spack'], method=GitOps.CLONE, collect_output=False).Run()
-        ModifyEnvNode(self.jarvis_env, f"spack/setup-env.sh", ModifyEnvNodeOps.REMOVE).Run()
+        ModifyEnvNode(self.jarvis_env, f".*spack/setup-env.sh", ModifyEnvNodeOps.REMOVE).Run()
         ModifyEnvNode(self.jarvis_env, f". {spack_root}/share/spack/setup-env.sh", ModifyEnvNodeOps.APPEND).Run()
 
     def _LocalUpdate(self):
         spack_root = os.environ['SPACK_ROOT']
-        GitNode(**self.config['spack'], method=GitOps.UPDATE, collect_output=False).Run()
+        GitNode(**self.config['spack'], path=spack_root, method=GitOps.UPDATE, collect_output=False).Run()
 
     def _LocalUninstall(self):
         spack_root = os.environ['SPACK_ROOT']
         shutil.rmtree(spack_root)
-        ModifyEnvNode(self.jarvis_env, f". {spack_root}/share/spack/setup-env.sh", ModifyEnvNodeOps.REMOVE).Run()
+        ModifyEnvNode(self.jarvis_env, f".*spack/setup-env.sh", ModifyEnvNodeOps.REMOVE).Run()
