559c97ae45ee64b3f58a5d41a40717b6609fc14f
lukemartinlogan <lukemartinlogan@gmail.com>
2022-07-11 04:11:55 -0500

    SPACK_ROOT env set

--- jarvis_cd/bootstrap/spack_setup.py
+++ jarvis_cd/bootstrap/spack_setup.py
@@ -8,7 +8,9 @@ class SpackSetup(Package):
     def _LocalInstall(self):
         spack_root = self.config['spack']['path']
         GitNode(**self.config['spack'], method=GitOps.CLONE, collect_output=False).Run()
+        ModifyEnvNode(self.jarvis_env, f"export SPACK_ROOT", ModifyEnvNodeOps.REMOVE).Run()
         ModifyEnvNode(self.jarvis_env, f".*spack/setup-env.sh", ModifyEnvNodeOps.REMOVE).Run()
+        ModifyEnvNode(self.jarvis_env, f"export SPACK_ROOT={spack_root}", ModifyEnvNodeOps.APPEND).Run()
         ModifyEnvNode(self.jarvis_env, f". {spack_root}/share/spack/setup-env.sh", ModifyEnvNodeOps.APPEND).Run()
 
     def _LocalUpdate(self):
@@ -17,4 +19,5 @@ class SpackSetup(Package):
     def _LocalUninstall(self):
         spack_root = self.config['spack']['path']
         shutil.rmtree(spack_root)
+        ModifyEnvNode(self.jarvis_env, f"export SPACK_ROOT", ModifyEnvNodeOps.REMOVE).Run()
         ModifyEnvNode(self.jarvis_env, f".*spack/setup-env.sh", ModifyEnvNodeOps.REMOVE).Run()
