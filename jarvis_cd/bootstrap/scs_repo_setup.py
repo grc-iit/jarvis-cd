463225ecc8b31bbe474257a31fa1f823e8696a6a
lukemartinlogan <lukemartinlogan@gmail.com>
2022-07-11 22:40:33 -0500

    Jarivs bootstap don't double up on ERR and OUT

--- jarvis_cd/bootstrap/scs_repo_setup.py
+++ jarvis_cd/bootstrap/scs_repo_setup.py
@@ -8,14 +8,14 @@ import shutil
 class SCSRepoSetup(Package):
     def _LocalInstall(self):
         scs_repo_root = self.config['scs_repo']['path']
-        GitNode(**self.config['scs_repo'], method=GitOps.CLONE, collect_output=False, print_output=True).Run()
+        GitNode(**self.config['scs_repo'], method=GitOps.CLONE, collect_output=False, print_output=True, print_fancy=False).Run()
         ExecNode(f'spack repo add {scs_repo_root}').Run()
         ModifyEnvNode(self.jarvis_env, f"export SCS_REPO", ModifyEnvNodeOps.REMOVE).Run()
         ModifyEnvNode(self.jarvis_env, f"export SCS_REPO={scs_repo_root}", ModifyEnvNodeOps.APPEND).Run()
 
     def _LocalUpdate(self):
         scs_repo_root = os.environ['SCS_REPO']
-        GitNode(**self.config['scs_repo'], method=GitOps.UPDATE, collect_output=False, print_output=True).Run()
+        GitNode(**self.config['scs_repo'], method=GitOps.UPDATE, collect_output=False, print_output=True, print_fancy=False).Run()
 
     def _LocalUninstall(self):
         scs_repo_root = os.environ['SCS_REPO']
--- jarvis_cd/bootstrap/spack_setup.py
+++ jarvis_cd/bootstrap/spack_setup.py
@@ -7,14 +7,14 @@ import shutil
 class SpackSetup(Package):
     def _LocalInstall(self):
         spack_root = self.config['spack']['path']
-        GitNode(**self.config['spack'], method=GitOps.CLONE, collect_output=False).Run()
+        GitNode(**self.config['spack'], method=GitOps.CLONE, collect_output=False, print_fancy=False).Run()
         ModifyEnvNode(self.jarvis_env, f"export SPACK_ROOT", ModifyEnvNodeOps.REMOVE).Run()
         ModifyEnvNode(self.jarvis_env, f".*spack/setup-env.sh", ModifyEnvNodeOps.REMOVE).Run()
         ModifyEnvNode(self.jarvis_env, f"export SPACK_ROOT={spack_root}", ModifyEnvNodeOps.APPEND).Run()
         ModifyEnvNode(self.jarvis_env, f". {spack_root}/share/spack/setup-env.sh", ModifyEnvNodeOps.APPEND).Run()
 
     def _LocalUpdate(self):
-        GitNode(**self.config['spack'], method=GitOps.UPDATE, collect_output=False).Run()
+        GitNode(**self.config['spack'], method=GitOps.UPDATE, collect_output=False, print_fancy=False).Run()
 
     def _LocalUninstall(self):
         spack_root = self.config['spack']['path']
