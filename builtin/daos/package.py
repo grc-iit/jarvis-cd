9612495b5c983ded1f0ce7cd8f89bc9f9985de11
lukemartinlogan <lukemartinlogan@gmail.com>
2022-08-08 17:03:43 -0500

    detect fabric_iface(s)

--- builtin/daos/package.py
+++ builtin/daos/package.py
@@ -54,7 +54,7 @@ class Daos(Application):
         #View network ifaces
         EchoNode("Detect Network Interfaces").Run()
         if 'fabric_iface' not in self.config['SERVER']['engines'][0] or self.config['SERVER']['engines']['fabric_iface'][0] is None:
-            if 'fabric_iface' not in self.config['SERVER']['engines'][0] or self.config['SERVER']['engines']['fabric_iface'][0] is None:
+            if 'fabric_ifaces' not in self.config['SERVER']['engines'][0] or self.config['SERVER']['engines']['fabric_ifaces'][0] is None:
                 DetectNetworks().Run()
                 iface = input('Initial Network Interface: ')
                 self.config['SERVER']['engines'][0]['fabric_iface'] = iface
