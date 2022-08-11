bb54d33a9903bb2dcfd41ca7ea58d43558a3955e
lukemartinlogan <lukemartinlogan@gmail.com>
2022-08-10 20:11:44 -0500

    Output cmds

--- jarvis_cd/shell/exec_node.py
+++ jarvis_cd/shell/exec_node.py
@@ -2,6 +2,8 @@ import sys
 from jarvis_cd.ssh.openssh.ssh_exec_node import SSHExecNode
 from jarvis_cd.shell.local_exec_node import LocalExecNode
 from jarvis_cd.parallel_node import ParallelNode
+from jarvis_cd.basic.echo_node import EchoNode
+from jarvis_cd.enumerations import Color
 
 sys.stderr = sys.__stderr__
 
@@ -23,6 +25,7 @@ class ExecNode(ParallelNode):
     def _Run(self):
         if len(self.cmds) == 0:
             return
+        EchoNode(self.cmds, color=Color.YELLOW).run()
         if self.do_ssh:
             self.node = SSHExecNode(self.cmds, **self.GetClassParams(ParallelNode, print_output=False)).Run()
         else:
