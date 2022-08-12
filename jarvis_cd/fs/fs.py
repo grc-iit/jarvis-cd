a88e4f1893eb4bd63714dbdd76f8445125c41849
lukemartinlogan <lukemartinlogan@gmail.com>
2022-08-12 12:24:34 -0500

    Add tmpfs, set sudo in kwargs

--- jarvis_cd/fs/fs.py
+++ jarvis_cd/fs/fs.py
@@ -5,19 +5,22 @@ import os
 class DropCaches(ExecNode):
     def __init__(self, **kwargs):
         cmd = "sh -c \"sync; echo 3 > /proc/sys/vm/drop_caches\""
-        super().__init__(cmd, sudo=True, **kwargs)
+        kwargs['sudo'] = True
+        super().__init__(cmd, **kwargs)
 
 class ChownFS(ExecNode):
     def __init__(self, fs_path, user=None, **kwargs):
         if user is None:
             user = os.environ['USER']
         cmd = f"chown -R {user} {fs_path}"
-        super().__init__(cmd, sudo=True, **kwargs)
+        kwargs['sudo'] = True
+        super().__init__(cmd, **kwargs)
 
 class UnmountFS(ExecNode):
     def __init__(self, dev_path, **kwargs):
         cmd = f"umount {dev_path}"
-        super().__init__(cmd, sudo=True, **kwargs)
+        kwargs['sudo'] = True
+        super().__init__(cmd, **kwargs)
 
 class MountFS(ExecNode):
     def __init__(self, dev_path, fs_path, dax=False, **kwargs):
@@ -29,22 +32,32 @@ class MountFS(ExecNode):
         if dax:
             cmd.append('-o dax')
         cmd = " ".join(cmd)
-        super().__init__(cmd, sudo=True, **kwargs)
+        kwargs['sudo'] = True
+        super().__init__(cmd, **kwargs)
 
 class EXT4Format(ExecNode):
     def __init__(self, dev_path, **kwargs):
         cmd = f"mkfs.ext4 {dev_path}"
-        super().__init__(cmd, sudo=True, **kwargs)
+        kwargs['sudo'] = True
+        super().__init__(cmd, **kwargs)
 
 class XFSFormat(ExecNode):
     def __init__(self, dev_path, **kwargs):
         cmd = f"mkfs.xfs {dev_path} -f"
-        super().__init__(cmd, sudo=True, **kwargs)
+        kwargs['sudo'] = True
+        super().__init__(cmd, **kwargs)
 
 class F2FSFormat(ExecNode):
     def __init__(self, dev_path, **kwargs):
         cmd = f"mkfs.f2fs {dev_path} -f"
-        super().__init__(cmd, sudo=True, **kwargs)
+        kwargs['sudo'] = True
+        super().__init__(cmd, **kwargs)
+
+class TmpfsFormat(ExecNode):
+    def __init_(self, size, mount_point, **kwargs):
+        cmd = f"mount -t tmpfs -o size={size} tmpfs {path}"
+        kwargs['sudo'] = True
+        super().__init__(cmd, **kwargs)
 
 class PrepareStorage(ExecNode):
     def __init__(self, spec, **kwargs):
@@ -54,13 +67,18 @@ class PrepareStorage(ExecNode):
         for item in self.spec:
             if item['format'] == 'EXT4':
                 nodes.append(EXT4Format(**item['format_params'], **kwargs))
-                nodes.append(MountFS(**item['mount_params'], **kwargs))
+                if 'mount_params' in item:
+                    nodes.append(MountFS(**item['mount_params'], **kwargs))
             if item['format'] == 'XFS':
                 nodes.append(XFSFormat(**item['format_params'], **kwargs))
-                nodes.append(MountFS(**item['mount_params'], **kwargs))
+                if 'mount_params' in item:
+                    nodes.append(MountFS(**item['mount_params'], **kwargs))
             if item['format'] == 'F2FS':
                 nodes.append(F2FSFormat(**item['format_params'], **kwargs))
-                nodes.append(MountFS(**item['mount_params'], **kwargs))
+                if 'mount_params' in item:
+                    nodes.append(MountFS(**item['mount_params'], **kwargs))
+            if item['format'] == 'tmpfs':
+                nodes.append(TmpfsFormat(**item['format_params'], **kwargs))
 
     def _Run(self):
         for node in self.nodes:
