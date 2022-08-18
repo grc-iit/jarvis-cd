3edcc9bd6ba6ff1c329bafba07a56dbe9c458065
lukemartinlogan <lukemartinlogan@gmail.com>
2022-08-18 08:10:18 -0500

    Add expandpath util

--- jarvis_cd/basic/yaml_conf.py
+++ jarvis_cd/basic/yaml_conf.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from jarvis_cd.basic.jarvis_manager import JarvisManager
 from jarvis_cd.serialize.yaml_file import YAMLFile
+from jarvis_cd.util.expand_paths import ExpandPaths
 import os
 
 from jarvis_cd.basic.exception import Error, ErrorCode
@@ -20,7 +21,7 @@ class YAMLConfig(ABC):
             os.environ['SHARED_DIR'] = str(self.config['SHARED_DIR'])
         if 'PER_NODE_DIR' in self.config and self.config['PER_NODE_DIR'] is not None:
             os.environ['PER_NODE_DIR'] = str(self.config['PER_NODE_DIR'])
-        self.config = self._ExpandPaths()
+        ExpandPaths(self.config)
         self._ProcessConfig()
         return self
 
@@ -35,28 +36,6 @@ class YAMLConfig(ABC):
         self.config = self._ExpandPaths()
         self._ProcessConfig()
         return self
-
-    def _ExpandPath(self, path):
-        return os.path.expandvars(path)
-
-    def _ExpandDict(self, dict_var):
-        return {key : self._ExpandVar(var) for key,var in dict_var.items()}
-
-    def _ExpandList(self, list_var):
-        return [self._ExpandVar(var) for var in list_var]
-
-    def _ExpandVar(self, var):
-        if isinstance(var, dict):
-            return self._ExpandDict(var)
-        if isinstance(var, list):
-            return self._ExpandList(var)
-        if isinstance(var, str):
-            return self._ExpandPath(var)
-        else:
-            return var
-
-    def _ExpandPaths(self):
-        return self._ExpandVar(self.config)
 
     @abstractmethod
     def _ProcessConfig(self):
