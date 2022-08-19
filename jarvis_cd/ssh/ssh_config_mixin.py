f1d16edd4c770bf15faf4323e451707c0d37c6c6
lukemartinlogan <lukemartinlogan@gmail.com>
2022-08-19 08:24:11 -0500

    Always check host aliases

--- jarvis_cd/ssh/ssh_config_mixin.py
+++ jarvis_cd/ssh/ssh_config_mixin.py
@@ -27,12 +27,11 @@ class SSHConfigMixin(YAMLConfig):
         self.ssh_info = FromOpenSSHConfig(self.all_hosts).Run().GetConfig()
         if 'SSH' in self.config and 'primary' in self.config['SSH']:
             self.ssh_info.update(self.config['SSH']['primary'])
-        if len(self.ssh_info):
-            if 'host_aliases' not in self.ssh_info:
-                self.ssh_info['host_aliases'] = []
-            self.ssh_info['host_aliases'] += FindHostAliases(self.all_hosts).Run().GetAliases()
-            EchoNode(f"This host has the following aliases: {self.ssh_info['host_aliases']}", color=Color.Yellow).Run()
-            self.host_aliases = self.ssh_info['host_aliases']
+        if 'host_aliases' not in self.ssh_info:
+            self.ssh_info['host_aliases'] = []
+        self.ssh_info['host_aliases'] += FindHostAliases(self.all_hosts).Run().GetAliases()
+        EchoNode(f"This host has the following aliases: {self.ssh_info['host_aliases']}", color=Color.Yellow).Run()
+        self.host_aliases = self.ssh_info['host_aliases']
         JarvisManager.GetInstance().SetSSHInfo(self.ssh_info)
 
     def _GetPublicKey(self, key_dir, key_name):
