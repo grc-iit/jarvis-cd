a124c073ee29594b1e439b0ad4735be56e2d246d
lukemartinlogan <lukemartinlogan@gmail.com>
2022-08-19 04:41:19 -0500

    Print jarvis exec command

--- jarvis_cd/shell/jarvis_exec_node.py
+++ jarvis_cd/shell/jarvis_exec_node.py
@@ -2,6 +2,7 @@
 from jarvis_cd.basic.parallel_node import ParallelNode
 from jarvis_cd.shell.exec_node import ExecNode
 from jarvis_cd.basic.echo_node import EchoNode
+from jarvis_cd.basic.enumerations import Color
 from abc import abstractmethod
 
 class JarvisExecNode(ParallelNode):
@@ -9,8 +10,8 @@ class JarvisExecNode(ParallelNode):
         if self.do_ssh:
             # We ignore ParallelNode since we don't want to call SSH during _LocalRun
             cmd = self._ToShellCmd(ignore_base=ParallelNode)
+            EchoNode(cmd, color=Color.YELLOW).Run()
             node = ExecNode(cmd, **self.GetClassParams(ParallelNode, print_output=False, shell=True)).Run()
-            #EchoNode(cmd).Run()
             self.output = node.GetOutput()
         else:
             self._LocalRun()
