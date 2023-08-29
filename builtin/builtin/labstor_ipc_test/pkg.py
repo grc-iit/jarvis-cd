9504651cf1bf0f0d5695b08cea38c59e4cc79c85
lukemartinlogan <lukemartinlogan@gmail.com>
2023-08-29 10:12:10 -0500

    Add hermes exec

--- builtin/builtin/labstor_ipc_test/pkg.py
+++ builtin/builtin/labstor_ipc_test/pkg.py
@@ -50,9 +50,16 @@ class LabstorIpcTest(Application):
 
         :return: None
         """
-        Exec(f'test_ipc_exec {self.config["TEST_CASE"]}',
-             MpiExecInfo(hostfile=self.jarvis.hostfile,
-                         env=self.env))
+        test_ipc_execs = ['TestIpc', 'TestIO']
+        test_hermes_execs = ['TestBucket']
+        if self.config['TEST_CASE'] in test_ipc_execs:
+            Exec(f'test_ipc_exec {self.config["TEST_CASE"]}',
+                 MpiExecInfo(hostfile=self.jarvis.hostfile,
+                             env=self.env))
+        elif self.config['TEST_CASE'] in test_hermes_execs:
+            Exec(f'test_hermes_exec {self.config["TEST_CASE"]}',
+                 MpiExecInfo(hostfile=self.jarvis.hostfile,
+                             env=self.env))
 
     def stop(self):
         """
