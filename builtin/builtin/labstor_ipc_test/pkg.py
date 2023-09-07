e91c030bbd6a22099a4b74bbcbe6327f7dc2974d
lukemartinlogan <lukemartinlogan@gmail.com>
2023-09-06 21:15:26 -0500

    Add more hermes tests

--- builtin/builtin/labstor_ipc_test/pkg.py
+++ builtin/builtin/labstor_ipc_test/pkg.py
@@ -51,7 +51,10 @@ class LabstorIpcTest(Application):
         :return: None
         """
         test_ipc_execs = ['TestIpc', 'TestIO']
-        test_hermes_execs = ['TestHermesPut', 'TestHermesPutGet']
+        test_hermes_execs = [
+            'TestHermesPut', 'TestHermesPutGet',
+            'TestHermesPartialPutGet', 'TestHermesBlobDestroy',
+            'TestHermesBucketDestroy', 'TestHermesReorganizeBlob']
         print(self.config['TEST_CASE'])
         if self.config['TEST_CASE'] in test_ipc_execs:
             Exec(f'test_ipc_exec {self.config["TEST_CASE"]}',
