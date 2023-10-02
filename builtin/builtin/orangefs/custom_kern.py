43c223db1438096ace389d6a6d52f7d229059895
lukemartinlogan <lukemartinlogan@gmail.com>
2023-10-02 03:47:03 -0500

    Separate storage space cmds

--- builtin/builtin/orangefs/custom_kern.py
+++ builtin/builtin/orangefs/custom_kern.py
@@ -8,6 +8,14 @@ class OrangefsCustomKern:
         for host in self.server_hosts.list():
             host_ip = host.hosts[0]
             server_start_cmds = [
+                f'pvfs2-server -f -a {host_ip}  {self.config["pfs_conf"]}'
+            ]
+            Exec(server_start_cmds, SshExecInfo(
+                hosts=host,
+                env=self.env))
+        for host in self.server_hosts.list():
+            host_ip = host.hosts[0]
+            server_start_cmds = [
                 f'pvfs2-server -f -a {host_ip}  {self.config["pfs_conf"]}',
                 f'pvfs2-server -a {host_ip} {self.config["pfs_conf"]}'
             ]
