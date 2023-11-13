8643890c5e56042e5c6efb5caa9f753e67e9db74
Luke Logan <llogan@pop-os.localdomain>
2023-11-12 22:13:51 -0700

    Remove coverage

--- setup.py
+++ setup.py
@@ -25,8 +25,8 @@ setuptools.setup(
     install_requires=[
         'pyyaml',
         'pylint==2.15.0',
-        'coverage==5.5',
-        'coverage-lcov==0.2.4',
+        # 'coverage==5.5',
+        # 'coverage-lcov==0.2.4',
         'pytest==6.2.5',
         'jarvis-util @ git+https://github.com/scs-lab/jarvis-util.git#egg=jarvis-util'
     ]
