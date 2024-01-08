778c7d9fa8255b3d83a478e95004e5f2262cb002
candiceT233 <candicet233@gmail.com>
2024-01-08 13:34:52 -0600

    single node sbatch working

--- builtin/builtin/arldm/pkg.py
+++ builtin/builtin/arldm/pkg.py
@@ -1,17 +1,15 @@
 """
-This module provides classes and methods to launch the Gray Scott application.
-Gray Scott is a 3D 7-point stencil code for modeling the diffusion of two
-substances.
+This module provides classes and methods to launch the Arldm application.
+Arldm is ....
 """
-from jarvis_cd.basic.pkg import Application, Color
+from jarvis_cd.basic.pkg import Application
 from jarvis_util import *
-import time
 import pathlib
+import yaml
 
-
-class ARLDM(Application):
+class Arldm(Application):
     """
-    This class provides methods to launch the ARLDM application.
+    This class provides methods to launch the Arldm application.
     """
     def _init(self):
         """
@@ -27,7 +25,6 @@ class ARLDM(Application):
 
         :return: List(dict)
         """
-        
         return [
             {
                 'name': 'conda_env',
@@ -39,13 +36,14 @@ class ARLDM(Application):
                 'name': 'config',
                 'msg': 'The config file for running analysis',
                 'type': str,
-                'default': f'{self.pkg_dir}/example_config/config_template.yaml',
+                'default': f'{self.pkg_dir}/example_config/config_template.yml',
             },
             {
                 'name': 'runscript',
                 'msg': 'The name of the ARLDM script to run',
                 'type': str,
                 'default': 'vistsis', # smallest dataset
+                'choices': ['flintstones', 'pororo', 'vistsis', 'vistdii'],
             },
             {
                 'name': 'flush_mem',
@@ -76,18 +74,14 @@ class ARLDM(Application):
                 'msg': 'Mode of running ARLDM: train(D) or sample',
                 'type': str,
                 'default': 'train',
-            },
-            {
-                'name': 'supported_runscripts',
-                'msg': 'List of supported running scripts',
-                'type': list,
-                'default': ['flintstones', 'pororo', 'vistsis', 'vistdii'],
+                'choice': ['train', 'sample'],
             },
             {
                 'name': 'num_workers',
                 'msg': 'Number of CPU workers to use for parallel processing',
                 'type': int,
                 'default': 1,
+                'choices': [0, 1, 2],
             },
             {
                 'name': 'experiment_path',
@@ -112,76 +106,28 @@ class ARLDM(Application):
                 'msg': 'HDF5 file to save samples',
                 'type': str,
                 'default': None, #'${HOME}/experiments/ARLDM/output_data/{runscript}.h5',
+            },
+            {
+                'name': 'prep_hdf5',
+                'msg': 'Prepare the HDF5 file for the ARLDM run',
+                'type': bool,
+                'default': False,
             }
         ]
 
-    def _configure(self, **kwargs):
-        """
-        Converts the Jarvis configuration to application-specific configuration.
-
-        :param kwargs: Configuration parameters for this pkg.
-        :return: None
-        """
-        
-        self.env['HDF5_USE_FILE_LOCKING'] = "FALSE" # set HDF5 locking: FALSE, TRUE, BESTEFFORT
-        
-        if self.config['experiment_path'] is not None:
-            self.config['experiment_path'] = os.path.expandvars(self.config['experiment_path'])
-            self.env['EXPERIMENT_PATH'] = self.config['experiment_path']
-        
-        if self.config['conda_env'] is None:
-            raise Exception("Must set the conda environment for running ARLDM")
-        if self.config['config'] is None:
-            raise Exception("Must set the ARLDM config file")
-        if self.config['runscript'] is None:
-            raise Exception("Must set the ARLDM script to run")
-        else:
-            # Check if runscript is supported
-            if self.config['runscript'] not in self.config['supported_runscripts']:
-                raise Exception(f"Runscript {self.config['runscript']} is not supported, 
-                                must be one of {self.config['supported_runscripts']}")
-            
-            
-        if self.config['flush_mem'] == False:
-            Exec(f"export FLUSH_MEM=FALSE")
-        else:
-            if self.config['flush_mem_cmd'] is None:
-                raise Exception("Must add the command to flush memory using flush_mem_cmd")
-        
-        if self.config['arldm_path'] is None:
-            raise Exception("Must set the path to the ARLDM source code")
-        else:
-            # check that path exists
-            pathlib.Path(self.config['arldm_path']).exists()
-            self.config['log_file'] = f'{self.config["arldm_path"]}/arldm_run.log'
-            self.config['stdout'] = f'{self.config["arldm_path"]}/arldm_run.log'
-        
-        # check and make -p experiment_path
-        pathlib.Path(self.config['experiment_path']).mkdir(parents=True, exist_ok=True)
-        
-        # set ckpt_dir
-        self.config['ckpt_dir'] = f'{self.config["experiment_path"]}/save_ckpt'
-        pathlib.Path(self.config['ckpt_dir']).mkdir(parents=True, exist_ok=True)
-        
-        # set sample_output_dir
-        self.config['sample_output_dir'] = f'{self.config["experiment_path"]}/output_data/sample_out_{self.config["runscript"]}_{self.config["mode"]}'
-        pathlib.Path(self.config['sample_output_dir']).mkdir(parents=True, exist_ok=True)
-        
-        # set sample_output_dir
-        self.config['hdf5_file'] = f'{self.config["experiment_path"]}/output_data/{self.config["runscript"]}.h5'
-        
-        
-        self._configure_yaml()
-        
-
     def _configure_yaml(self):
         yaml_file = self.config['config']
+
+        if "_template.yml" not in str(yaml_file):
+            yaml_file = yaml_file.replace(".yml", "_template.yml")
         
+        self.log(f"ARLDM yaml_file: {yaml_file}")
+
         with open(yaml_file, "r") as stream:
             try:
                 config_vars = yaml.safe_load(stream)
                 
-                run_test = config_vars['runscript']
+                run_test = self.config['runscript']
                 
                 config_vars['mode'] = self.config['mode']
                 config_vars['num_workers'] = self.config['num_workers']
@@ -199,12 +145,139 @@ class ARLDM(Application):
                 print(exc)
         self.config['config'] = new_yaml_file
 
+    def _configure(self, **kwargs):
+        """
+        Converts the Jarvis configuration to application-specific configuration.
+        E.g., OrangeFS produces an orangefs.xml file.
+
+        :param kwargs: Configuration parameters for this pkg.
+        :return: None
+        """
+        self.env['HDF5_USE_FILE_LOCKING'] = "FALSE" # set HDF5 locking: FALSE, TRUE, BESTEFFORT
+        self.env['HYDRA_FULL_ERROR'] = "1" # complete stack trace.
+        
+        if self.config['experiment_path'] is not None:
+            self.config['experiment_path'] = os.path.expandvars(self.config['experiment_path'])
+            self.env['EXPERIMENT_PATH'] = self.config['experiment_path']
+        
+        if self.config['conda_env'] is None:
+            raise Exception("Must set the conda environment for running ARLDM")
+        if self.config['config'] is None:
+            raise Exception("Must set the ARLDM config file")
+        if self.config['runscript'] is None:
+            raise Exception("Must set the ARLDM script to run")
+            
+        if self.config['flush_mem'] == False:
+            self.env['FLUSH_MEM'] = "FALSE"
+        else:
+            if self.config['flush_mem_cmd'] is None:
+                raise Exception("Must add the command to flush memory using flush_mem_cmd")
+        
+        if self.config['arldm_path'] is None:
+            raise Exception("Must set the path to the ARLDM source code")
+        else:
+            # check that path exists
+            pathlib.Path(self.config['arldm_path']).exists()
+            # no default log file
+            # self.config['log_file'] = f'{self.config["arldm_path"]}/arldm_run.log'
+            # self.config['stdout'] = f'{self.config["arldm_path"]}/arldm_run.log'
+        
+        # check and make -p experiment_path
+        pathlib.Path(self.config['experiment_path']).mkdir(parents=True, exist_ok=True)
+        
+        # set ckpt_dir
+        self.config['ckpt_dir'] = f'{self.config["experiment_path"]}/save_ckpt'
+        pathlib.Path(self.config['ckpt_dir']).mkdir(parents=True, exist_ok=True)
+        
+        # set sample_output_dir
+        self.config['sample_output_dir'] = f'{self.config["experiment_path"]}/output_data/sample_out_{self.config["runscript"]}_{self.config["mode"]}'
+        pathlib.Path(self.config['sample_output_dir']).mkdir(parents=True, exist_ok=True)
+        
+        # set sample_output_dir
+        self.config['hdf5_file'] = f'{self.config["experiment_path"]}/output_data/{self.config["runscript"]}.h5'
+        
+        self._configure_yaml()
+
     def prep_hdf5_file(self):
         """
         Prepare the HDF5 file for the ARLDM run
         """
+        
+        cmd = [
+            'python',
+        ]
+        
+        if self.config['runscript'] == 'pororo':
+            cmd.append(f'{self.config["arldm_path"]}/data_script/pororo_hdf5.py')
+            cmd.append(f'--data_dir {self.config["experiment_path"]}/input_data/pororo_png')
+            cmd.append(f'--save_path {self.config["hdf5_file"]}')
+        elif self.config['runscript'] == 'flintstones':
+            cmd.append(f'{self.config["arldm_path"]}/data_script/flintstones_hdf5.py')
+            cmd.append(f'--data_dir {self.config["experiment_path"]}/input_data/flintstones_data')
+            cmd.append(f'--save_path {self.config["hdf5_file"]}')
+        elif self.config['runscript'] == 'vistsis' or self.config['runscript'] == 'vistdii':
+            cmd.append(f'{self.config["arldm_path"]}/data_script/vist_hdf5.py')
+            cmd.append(f'--sis_json_dir {self.config["arldm_path"]}/input_data/sis')
+            cmd.append(f'--dii_json_dir {self.config["arldm_path"]}/input_data/dii')
+            cmd.append(f'--img_dir {self.config["experiment_path"]}/input_data/visit_img')
+            cmd.append(f'--save_path {self.config["hdf5_file"]}')
+        else:
+            raise Exception("Must set the correct ARLDM script to run")
+        
+        prep_cmd = ' '.join(cmd)
+        Exec(prep_cmd,
+                LocalExecInfo(env=self.mod_env,))
+        
         pass
+    
+    def train(self):
+        """
+        Run the ARLDM training run
+        """
+        # # check which python is being used
+        # Exec("which python", LocalExecInfo(env=self.mod_env,))
+        print(f"ARLDM training run: {self.config['runscript']}")
+        
+        cmd = [
+            f"cd {self.config['arldm_path']}; echo Executing from directory `pwd`;",
+            'conda','run', '-n', self.config['conda_env'], # conda environment
+            'python',
+        ]
 
+        if self.config['arldm_path'] and self.config['runscript']:
+            cmd.append(f'{self.config["arldm_path"]}/main.py')
+        
+        conda_cmd = ' '.join(cmd)
+        
+        print(f"Running ARLDM with command: {conda_cmd}")
+        
+        # Move config file to arldm_path
+        Exec(f"cp {self.config['config']} {self.config['arldm_path']}/config.yml",
+             LocalExecInfo(env=self.mod_env,))
+        
+        # # go to arldm_path
+        # Exec(f"cd {self.config['arldm_path']}",
+        #      LocalExecInfo(env=self.mod_env,))
+        
+        # Exec(f"cd {self.config['arldm_path']}; echo Executing from directory `pwd`", LocalExecInfo(env=self.mod_env,))
+        
+        start = time.time()
+        
+        Exec(conda_cmd,
+             LocalExecInfo(env=self.mod_env,))
+        
+        end = time.time()
+        diff = end - start
+        self.log(f'TIME: {diff} seconds') # color=Color.GREEN
+    
+    def sample(self):
+        """
+        Run the ARLDM sampling run
+        
+        This step can only be run when training is fully completed. 
+        Currently train is set to fast_dev_run.
+        """
+        print(f"ARLDM sampling run: not implemented yet")
 
     def start(self):
         """
@@ -213,34 +286,16 @@ class ARLDM(Application):
 
         :return: None
         """
-        pass
+        if self.config['prep_hdf5']:
+            self.prep_hdf5_file()
         
-        # check which python is being used
-        Exec("which python")
+        if self.config['mode'] == 'train':
+            self.train()
         
-        # cmd = [
-        #     'conda','run', '-n', self.config['conda_env'], # conda environment
-        #     'python',
-        # ]
+        if self.config['mode'] == 'sample':
+            self.sample()
+        
 
-        # if self.config['arldm_path'] and self.config['runscript']:
-        #     cmd.append(f'{self.config["arldm_path"]}/main.py')
-        
-        # conda_cmd = ' '.join(cmd)
-        
-        # print(f"Running ARLDM with command: {conda_cmd}")
-        # print(f"STDOUT to : {self.config['log_file']}")
-        
-        # start = time.time()
-        
-        # Exec(conda_cmd,
-        #      LocalExecInfo(env=self.mod_env,
-        #                    pipe_stdout=self.config['log_file'],))
-        
-        # end = time.time()
-        # diff = end - start
-        # self.log(f'TIME: {diff} seconds') # color=Color.GREEN
-        
 
     def stop(self):
         """
@@ -259,6 +314,3 @@ class ARLDM(Application):
         :return: None
         """
         pass
-        # output_dir = self.config['output'] + "*"
-        # print(f'Removing {output_dir}')
-        # Rm(output_dir)
