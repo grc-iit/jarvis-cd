8f80a509181ef911977bea854039f6634a9e9c27
lukemartinlogan <lukemartinlogan@gmail.com>
2025-03-20 23:35:09 -0500

    Adapt hermes pkg to use new find_net_info api

--- builtin/builtin/hermes/pkg.py
+++ builtin/builtin/hermes/pkg.py
@@ -137,10 +137,12 @@ class Hermes(Service):
                                       env=self.env))
 
         # Get network Info
-        if len(hosts) > 1:
-            net_info = rg.find_net_info(hosts, shared=True)
-        else:
-            net_info = rg.find_net_info(hosts)
+        net_info = rg.find_net_info(self.jarvis.hostfile, strip_ips=True, 
+                                    local=len(hosts) == 1, env=self.env)
+        # if len(hosts) > 1:
+        #     net_info = rg.find_net_info(hosts, shared=True)
+        # else:
+        #     net_info = rg.find_net_info(hosts)
         if len(net_info) == 0:
             raise Exception(f'Failed to find any networks')
         provider = self.config['provider']
