41bc37a68e51e864a56dd4d11e4440b3e14982d7
lukemartinlogan <lukemartinlogan@gmail.com>
2026-04-17 22:37:58 -0500

    Set up SSH in CI for localhost testing
    Configure passwordless SSH to localhost in CI environment by:
    - Installing openssh-server and openssh-client packages
    - Generating RSA keypair and configuring authorized_keys
    - Starting SSH daemon
    - Adding localhost to known_hosts to prevent prompts
    
    This enables SSH-based tests (SshExec, PsshExec) to run in CI without needing
    remote hosts or managing external SSH configuration.

--- ci/install_deps.sh
+++ ci/install_deps.sh
@@ -2,4 +2,15 @@
 set -e
 
 sudo apt-get update -y
-sudo apt-get install -y mpich
+sudo apt-get install -y mpich openssh-server openssh-client
+
+# Set up passwordless SSH to localhost for tests
+ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa -q
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+chmod 600 ~/.ssh/authorized_keys
+
+# Start SSH daemon
+sudo service ssh start
+
+# Add localhost to known_hosts so SSH doesn't prompt
+ssh-keyscan -H localhost >> ~/.ssh/known_hosts
